f78ce3a5810e5c88013cc0635159f7821076b4d6
manhtuyen1111 <157588104+manhtuyen1111@users.noreply.github.com>
2026-02-09 15:11:32 +0700

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -28,31 +28,6 @@ export const USERS: Record<string, User> = {
     name: 'QC Manager', 
     role: 'qc' 
   },
-  'qc01': { 
-    username: 'qc01', 
-    name: 'QC Tổ 1', 
-    role: 'worker', 
-    assignedTeamId: 't1' 
-  },
-  'qc02': { 
-    username: 'qc02', 
-    name: 'QC Tổ 2', 
-    role: 'worker', 
-    assignedTeamId: 't2' 
-  },
-  'qc03': { 
-    username: 'qc03', 
-    name: 'QC Tổ 3', 
-    role: 'worker', 
-    assignedTeamId: 't3' 
-  },
-  'qc04': { 
-    username: 'qc04', 
-    name: 'QC Tổ 4', 
-    role: 'worker', 
-    assignedTeamId: 't4' 
-  },
-
   // ────────────── TÀI KHOẢN MỚI ──────────────
   'thi.nb': { 
     username: 'Nguyễn Bá Thi', 
