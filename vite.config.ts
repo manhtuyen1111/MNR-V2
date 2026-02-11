88b14f973f7a46627dc0720dc6a4d97fd89dde52
manhtuyen1111 <157588104+manhtuyen1111@users.noreply.github.com>
2026-02-11 10:38:11 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
+import { VitePWA } from 'vite-plugin-pwa'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+  react(),
+  VitePWA({
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Container QC App',
+      short_name: 'QC',
+      theme_color: '#ffffff',
+      background_color: '#ffffff',
+      display: 'standalone',
+      icons: [
+        {
+          src: '/icon-192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: '/icon-512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    }
+  })
+],
