a34eecd49054cce98194e33a649e5545b92068bb
manhtuyen1111 <157588104+manhtuyen1111@users.noreply.github.com>
2026-02-23 19:57:13 +0700

    Update HistoryList.tsx

--- components/HistoryList.tsx
+++ components/HistoryList.tsx
@@ -214,7 +214,7 @@ const HistoryList: React.FC<HistoryListProps> = ({
 
                   {/* META */}
                   <div className="text-[10.5px] text-slate-400 whitespace-nowrap">
-                    {r.teamName} {r.images.length}pic{' '}
+                    {r.teamName}
                     {formatDate(r.timestamp)}
                   </div>
                 </div>
