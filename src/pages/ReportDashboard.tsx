43424380e12f798b45f31c0505377971fa797da5
manhtuyen1111 <157588104+manhtuyen1111@users.noreply.github.com>
2026-02-24 19:11:01 +0700

    Update ReportDashboard.tsx

--- src/pages/ReportDashboard.tsx
+++ src/pages/ReportDashboard.tsx
@@ -138,7 +138,7 @@ const ReportDashboard = () => {
           >
             <path d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4" />
           </svg>
-          BÁO CÁO TỔNG HỢP
+          BÁO CÁO TỔNG HỢP NGHIỆM THU MNR MATRAN 2026 
         </h1>
 
         {/* Range selector - Dropdown */}
