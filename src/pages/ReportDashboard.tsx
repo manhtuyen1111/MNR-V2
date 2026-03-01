1d40f39975e20ae195dc5cdbf2eb9b24bed41946
manhtuyen1111 <157588104+manhtuyen1111@users.noreply.github.com>
2026-03-01 18:52:49 +0700

    Update ReportDashboard.tsx

--- src/pages/ReportDashboard.tsx
+++ src/pages/ReportDashboard.tsx
@@ -46,10 +46,17 @@ const ReportDashboard = () => {
   const [loading, setLoading] = useState(false);
 
   const [selectedTeam, setSelectedTeam] = useState("ALL");
-  const formatLocalDate = (date: Date) => {
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const day = String(date.getDate()).padStart(2, "0");
+ const formatLocalDate = (date: Date) => {
+  const local = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate()
+  );
+
+  const year = local.getFullYear();
+  const month = String(local.getMonth() + 1).padStart(2, "0");
+  const day = String(local.getDate()).padStart(2, "0");
+
   return `${year}-${month}-${day}`;
 };
   const [rangeType, setRangeType] = useState("THIS_MONTH");
